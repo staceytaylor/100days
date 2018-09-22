4abe53335d0eada5d4b769f9bcfa6e4c6d26c162
staceytaylor <33071840+staceytaylor@users.noreply.github.com>
2018-09-22 19:09:01 -0300

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,31 @@
+# September 22, 2018
+# I have no code to share yet. I spent my 1 hour getting myself set up for this challenge. I never
+# anticipated sharing it (at this point) so publicly, but I figured, it's go big or go home. So yeah.
+
+# For me, NLP is where it's at. I love it (and hate it when I can't get my code to run haha).
+
+# My project that I will be working on is for my thesis. So, you will see my code evolve over time. 
+# I am going to be applying it to the realm of accounting. I see so many applications for NLP
+# there and I think it will take me into some interesting places.
+
+# If there is a hard way of going about ANY career, that's the way I have done mine. When people 
+# look at my resume, my education, and my experience, they usually end up with a very puzzled look
+# on their face. And of course, that whole "do it the hard way" thing also applies to my coding. 
+# Some of it, I'm pretty good at. Some of it, not so much... I am very proud of all of my career
+# experiences and I wouldn't do it any differently, if given the chance. Someone recently asked me
+# why I would give up my good paying (meaning my "respectable") industry job to go back to 
+# school with my sights set on joining Academia. In the ensuing conversation, it became clear that 
+# the people I was talking with saw it as such a waste. I certainly don't see it as giving up 
+# anything, let alone it being a waste. We all have our races to run and I have chosen, either
+# consciously or unconsciously through my decisions, to run mine differently. Yes, I struggle with
+# coding. But, there are many aspects of the greater purpose of the coding where I don't struggle
+# but others do. That is where I feel that I can make the biggest contribution at the moment -
+# to help others understand and see the bigger pictures, just like they can help me understand and
+# see better ways to go about my coding. So even though I am not new new (no, I didin't put in two
+# "new" by accident there...) to coding, I am going to use these 100 days to become more proficient
+# at it by working on improving my code, asking questions, forging new relationships, and hope that 
+# I will feel more comfortable in the community I am in due to my school program, to the point where # I feel that I belong.   
+
+print "I can do this."
+
+# hey, I coulda said "hello world!" instead lol
